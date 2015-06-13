e49fdf6e1c94880f75a35fc305efcd3ef5dd9971
Vot Z <wojciech.zardzewialy@gmail.com>
2015-06-13 01:26:53 +0100

    ROOT_DIR now detected dynamically to allow using npm-toolkit in child directories

--- constants.js
+++ constants.js
@@ -1,9 +1,28 @@
+var fs = require('fs');
+var path = require('path');
+var debug = false;
+
 var constants = {
   NPM_TOOLKIT_VERSION: '0.0.1',
-  ROOT_DIR: process.cwd(),
   SETTINGS_DIR_NAME: 'npm-toolkit-rc',
 };
 
-constants.SETTINGS_DIR = [constants.ROOT_DIR, constants.SETTINGS_DIR_NAME].join('/');
+var resolveRootDir = function (checkDir) {
+  if (fs.existsSync(checkDir + '/' + constants.SETTINGS_DIR_NAME)) {
+    if (debug) console.log(constants.SETTINGS_DIR_NAME + ' found in ' + checkDir);
+    constants.ROOT_DIR = checkDir;
+    constants.SETTINGS_DIR = [checkDir, constants.SETTINGS_DIR_NAME].join('/');
+  } else {
+    if (debug) console.log(constants.SETTINGS_DIR_NAME + ' NOT found in ' + checkDir);
+    if (checkDir === '/') {
+      console.log('Couldn\'t find ' + constants.SETTINGS_DIR_NAME + '. Check if you\'re in the right directory.');
+      return process.exit(1);
+    }
+    var newDir = path.join(checkDir + '/..');
+    resolveRootDir(newDir);
+  }
+};
+
+resolveRootDir(process.cwd().toString());
 
 module.exports = constants;
