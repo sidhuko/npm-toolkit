ef0e90614805482b748e4e22c4206348c4c5f28b
Vot Z <vzardzewialy@cambridge.org>
2016-01-29 16:33:46 +0000

    Registering the tasks without requiring the 'do' prefix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 
 // Define dependencies
 var parser = require('nomnom')();
@@ -26,46 +26,58 @@ var Application = function () {
     .option('settings', {
       default: _.get(config, 'constants.settingsDir', 'npm-toolkit-rc'),
       help: 'Name of the settings directory'
-    })
-    .option('version', {
-      flag: true,
-      abbr: 'v',
-      help: 'Prints version',
-      callback: function() {
-        return 'npm-toolkit ' + _.get(config, 'constants.version', '')
-      }
+    // })
+    // .option('debug', {
+    //   default: false,
+    //   help: 'Enable debug mode'
+    // })
+    // .option('version', {
+    //   flag: true,
+    //   abbr: 'v',
+    //   help: 'Prints version',
+    //   callback: function() {
+    //     return 'npm-toolkit ' + _.get(config, 'constants.version', '');
+    //   }
     });
 
-  parser.command('web').callback(Commands.web);
 
-  parser.command('do').callback(Commands.do);
-
-  parser.command('make').callback(Commands.make);
-
+  // register tasks
   parser.command('status').callback(Commands.status);
-
-  parser.command('info').callback(Commands.status);
-
   parser.command('list').callback(Commands.list);
 
-  parser.command('init')
-    .callback(Commands.init)
-    .option('example', {
-      position: 0,
-      help: 'Use example data'
-    });
+  // keeping the "do" command for backwards compatibility.
+  // To be phased out in the next minor versions.
+  parser.command('do').callback(Commands.do);
 
-  parser.command('help')
-    .callback(Commands.help);
+  // parser.command('make').callback(Commands.make);
 
-  parser.nocommand()
-    .callback(function (options) {
-      Commands.cli();
-    })
+  // parser.command('init')
+  //   .callback(Commands.init)
+  //   .option('example', {
+  //     position: 0,
+  //     help: 'Use example data'
+  //   });
+
+  parser.command('help').callback(Commands.help);
+
+  // var debug = true;
+  var debug = false;
+  _.each(Helpers.scanTasks(config.paths.settings), function (fn, name) {
+    if (typeof fn === 'function') {
+      if (debug) console.log('Registering the "' + name + '" command');
+      parser.command(name).callback(fn);
+    }
+  });
+
+
+  // Fallback to CLI if no arguments provided
+  parser.nocommand().callback(Commands.cli);
+
 
   // Run parser and return user input
   return parser.parse();
 };
+
 
 // Execute the application
 module.exports = Application();
