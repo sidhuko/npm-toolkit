61859f0b2fdf11031b50cd85937dcfa29ccce0c8
Vot Z <vot@siliconfen.co>
2016-03-30 02:07:53 +0100

    Error handling + cleanup

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 #!/usr/bin/env node
-
 var parseCliArgs = require('./lib/parseCliArgs');
 var taskDispatcher = require('./lib/dispatcher');
 
-var runningDirectly = (require.main === module);
-// Helpers.ulimit.set(2048);
-
-// var chalk = require('chalk');
-// var fs = require('fs');
-// var _ = require('lodash');
-// var Commands = require('./lib/commands');
-// var Helpers = require('./lib/helpers');
-// var cliInterface = require('./cli');
-// var config = require('./config');
-
-
-// don't do this for nt init or nt install
-// var ntrcFound = config.initialise();
-
-
-
-
-// console.log('runningDirectly', runningDirectly);
-
-if (runningDirectly) {
+if (require.main === module) {
   var args = parseCliArgs();
   return taskDispatcher(args);
 } else {
