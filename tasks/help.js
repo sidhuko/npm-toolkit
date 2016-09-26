b16f95421d1f4991920b767912cfe5ba7430fa35
Vot <vot@siliconfen.co>
2016-09-26 10:21:50 +0100

    Updated help screen

--- tasks/help.js
+++ tasks/help.js
@@ -2,25 +2,28 @@ var chalk  = require('chalk');
 var Helpers = require('../lib/helpers');
 
 module.exports = function () {
-  Helpers.printHeader();
+  // Helpers.printHeader();
 
-  console.log(chalk.bold('TL;DR: npm-toolkit is essentially a fancy task runner.'));
-  console.log('It is written in a way that gives you all the flexibility you need');
-  console.log('to create custom workflows available in multiple interfaces.');
+  console.log(chalk.bold('npm-toolkit - flexible script launcher / task runner.'));
+  console.log('TL;DR: Allows you to create custom workflows that suit your project.');
+  console.log('Like Grunt or Gulp but without imposing a lot of their structure.');
   console.log();
-  console.log(chalk.bold('You can run npm-toolkit in two ways:'));
-  console.log(' * with an interactive interface (' + chalk.bold('nt') + ')');
-  console.log(' * execute commands directly (' + chalk.bold('nt [command name]') + ')');
+  // console.log(chalk.bold('You can run npm-toolkit in two ways:'));
+  // console.log(' * with an interactive interface (' + chalk.bold('nt') + ')');
+  // console.log(' * execute commands directly (' + chalk.bold('nt [command name]') + ')');
+  // console.log();
+  console.log(chalk.bold('System tasks:'));
+  console.log(' * nt help              This screen');
+  console.log(' * nt init              Initialises nt project in current location');
+  console.log(' * nt list (ls)         List project tasks');
+  console.log(' * nt status            Environment information');
+  console.log(' * nt version           Returns version number');
   console.log();
   console.log(chalk.bold('You can also pass the arguments to customise the behaviour:'));
-  console.log(' * --verbose or -v for a more detailed output');
-  console.log(' * --env=[file] for an alternative environmetal variables file to be loaded');
-  console.log(' * --config=[dirname] for a custom config directory (ntrc)');
+  console.log(' * --verbose      (-v)  for a more detailed output');
+  console.log(' * --env=[name]   (-e)  for an alternative environmetal variables file to be loaded');
+  console.log(' * --config=[dir] (-c)  for a custom config directory (ntrc)');
   console.log();
-  console.log(chalk.bold('More information:'));
-  console.log(' * nt help: This screen');
-  console.log(' * nt list: View available commands');
-  console.log(' * nt status: Environment information');
 
-  Helpers.printLine();
+  // Helpers.printLine();
 };
